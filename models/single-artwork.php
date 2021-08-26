6f39bd5f39bffe4df2fd3e188b5c4f9cbb553d73
Viljami Hirvonen <viljami.hirvonen@geniem.com>
2021-08-26 09:33:19 +0300

    Add name & location info groups

--- models/single-artwork.php
+++ models/single-artwork.php
@@ -8,6 +8,7 @@ use TMS\Theme\Base\Formatters\ImageFormatter;
 use TMS\Theme\Base\Traits;
 use TMS\Theme\Muumimuseo\PostType\Artist;
 use TMS\Theme\Muumimuseo\PostType\Artwork;
+use TMS\Theme\Muumimuseo\Taxonomy\ArtworkLocation;
 
 /**
  * The SingleArtwork class.
@@ -36,6 +37,57 @@ class SingleArtwork extends SingleArtist {
     public function additional_info() {
         $info_rows = ! empty( get_field( 'additional_information' ) ) ? get_field( 'additional_information' ) : [];
 
+        $artist_location_group = $this->get_info_group_location();
+
+        if ( ! empty( $artist_location_group ) ) {
+            array_unshift(
+                $info_rows,
+                $artist_location_group
+            );
+        }
+
+        $artist_name_group = $this->get_info_group_artist_name();
+
+        if ( ! empty( $artist_name_group ) ) {
+            array_unshift(
+                $info_rows,
+                $artist_name_group
+            );
+        }
+
+
+        return $info_rows;
+    }
+
+
+    /**
+     * Get info group artwork location.
+     *
+     * @return array[]|null
+     */
+    protected function get_info_group_location() : ?array {
+        $row = null;
+
+        $location_terms = wp_get_post_terms( get_the_ID(), ArtworkLocation::SLUG, [ 'fields' => 'names' ] );
+
+        if ( ! empty( $location_terms ) && ! is_wp_error( $location_terms ) ) {
+            $row = $this->format_info_group(
+                _x( 'Location', 'theme-frontend', 'tms-theme-muumimuseo' ),
+                implode( ', ', $location_terms )
+            );
+        }
+
+        return $row;
+    }
+
+    /**
+     * Get info group artist name.
+     *
+     * @return array[]|null
+     */
+    protected function get_info_group_artist_name() : ?array {
+        $row = null;
+
         if ( ! empty( get_field( 'artists' ) ) ) {
             $artists      = get_field( 'artists' );
             $artist_names = [];
@@ -51,22 +103,31 @@ class SingleArtwork extends SingleArtist {
 
             if ( ! empty( $artist_names ) ) {
                 $row_title = count( $artist_names ) > 1
-                    ? _x( 'Artists', 'theme-frontend', 'tms-theme-base' )
-                    : _x( 'Artist', 'theme-frontend', 'tms-theme-base' );
+                    ? _x( 'Artists', 'theme-frontend', 'tms-theme-muumimuseo' )
+                    : _x( 'Artist', 'theme-frontend', 'tms-theme-muumimuseo' );
 
-                array_unshift(
-                    $info_rows,
-                    [
-                        'additional_information_group' => [
-                            'additional_information_title' => $row_title,
-                            'additional_information_text'  => implode( ', ', $artist_names ),
-                        ],
-                    ]
-                );
+                $row = $this->format_info_group( $row_title, implode( ', ', $artist_names ) );
             }
         }
 
-        return $info_rows;
+        return $row;
+    }
+
+    /**
+     * Format data for use as additional information row.
+     *
+     * @param string $row_title Info group title.
+     * @param string $row_text  Info group text content.
+     *
+     * @return array[]
+     */
+    protected function format_info_group( string $row_title, string $row_text ) : array {
+        return [
+            'additional_information_group' => [
+                'additional_information_title' => $row_title,
+                'additional_information_text'  => $row_text,
+            ],
+        ];
     }
 
     /**
