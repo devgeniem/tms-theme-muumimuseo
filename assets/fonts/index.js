ec35cbe8f72dbebac862628af82caa34f207c865
Sami Harju <sami.harju@geniem.com>
2021-10-21 10:36:42 +0300

    Add support for Japanese

--- assets/fonts/index.js
+++ assets/fonts/index.js
@@ -1,3 +1,4 @@
+// Latin font families
 import './bodoni-egyptian-pro-bold.otf';
 import './bodoni-egyptian-pro-medium.otf';
 import './gotham-pro-bold.otf';
@@ -5,3 +6,9 @@ import './gotham-pro-light.otf';
 import './gotham-pro-regular.otf';
 import './gotham-pro-medium.otf';
 import './tovescript-regular.otf';
+
+// Japanese font families
+import './SourceHanSans-Bold.otf';
+import './SourceHanSans-Light.otf';
+import './SourceHanSans-Medium.otf';
+import './SourceHanSans-Regular.otf';
