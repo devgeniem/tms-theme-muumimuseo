45c8b66c131c89372a32fec05576d611e3f3be90
Timo Haapala <timo.haapala@geniem.com>
2021-11-03 13:42:25 +0200

    filter for overwriting max length

--- lib/ACFController.php
+++ lib/ACFController.php
@@ -5,7 +5,9 @@
 
 namespace TMS\Theme\Muumimuseo;
 
+use Closure;
 use \TMS\Theme\Base;
+use Geniem\ACF\Field\Textarea;
 /**
  * Class ACFController
  *
@@ -20,5 +22,20 @@ class ACFController extends Base\ACFController implements Base\Interfaces\Contro
      */
     protected function get_base_dir() : string {
         return __DIR__ . '/ACF';
+    }
+
+    // filter for overwriting grid field's properties
+    public function hooks() : void{
+        add_filter('tms/acf/grid_fields',Closure::fromCallable( [ $this, 'modify_acf'] ));
+    }
+
+    private function modify_acf($fields){
+        $description = $fields->sub_fields['description'];
+        if(empty($description) || !$description instanceof Textarea){
+            return $fields;
+        }
+        $description->set_maxlength( 300 );
+        $fields->sub_fields['description'] = $description;
+        return $fields;
     }
 }
